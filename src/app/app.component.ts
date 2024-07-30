e33c71eb7cd7ea70c698d0a902e235dec7bf5bde
Harshit <harshit.3851@gmail.com>
2024-07-30 23:43:27 +0530

    added form in contact page

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,16 +3,22 @@ import { RouterOutlet } from '@angular/router';
 import {MatButtonToggleModule} from '@angular/material/button-toggle'; 
 import { FormsModule } from '@angular/forms';
 import { CommonModule } from '@angular/common';
+import { MatButtonModule } from '@angular/material/button';
+import { MatInputModule } from '@angular/material/input';
+import { MatIconModule } from '@angular/material/icon';
+import { MatFormFieldModule } from '@angular/material/form-field';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet,MatButtonToggleModule,FormsModule,CommonModule],
+  imports: [RouterOutlet,MatButtonToggleModule,FormsModule,CommonModule,MatInputModule,MatIconModule,MatFormFieldModule,MatButtonModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
 export class AppComponent {
-  chosenLink ?:string ;
+chosenLink ?:string = "home";
+name?:string;
+email?:string;
   toggleButtonArray =[
     {
       "value":"home",
