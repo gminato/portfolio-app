de0f89d4ab06042c9665c2ff520b001e879d351e
Harshit <harshit.3851@gmail.com>
2024-07-30 16:39:19 +0530

    initial commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,34 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import {MatButtonToggleModule} from '@angular/material/button-toggle'; 
+import { FormsModule } from '@angular/forms';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet,MatButtonToggleModule,FormsModule,CommonModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
 export class AppComponent {
-  title = 'portfolio-app';
+  chosenLink ?:string ;
+  toggleButtonArray =[
+    {
+      "value":"home",
+      "title":"Home"
+    },
+    {
+      "value":"about",
+      "title":"About",
+    },
+    {
+      "value":"projects",
+      "title":"Projects"
+    },
+    {
+      "value":"contact",
+      "title":"Contact"
+    }
+  ] 
 }
