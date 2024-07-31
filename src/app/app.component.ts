f3239ac62a1defb8e393692e7fa73596dea4f676
Harshit <harshit.3851@gmail.com>
2024-07-31 20:30:21 +0530

    contact page changs

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,18 +7,31 @@ import { MatButtonModule } from '@angular/material/button';
 import { MatInputModule } from '@angular/material/input';
 import { MatIconModule } from '@angular/material/icon';
 import { MatFormFieldModule } from '@angular/material/form-field';
+import { NgIconComponent, provideIcons } from '@ng-icons/core';
+import { hugeNewTwitter } from '@ng-icons/huge-icons'
+import { hugeGithub01 } from '@ng-icons/huge-icons'
+import { hugeLinkedin02 } from '@ng-icons/huge-icons'
+import { hugeLeetcode } from '@ng-icons/huge-icons'
+
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet,MatButtonToggleModule,FormsModule,CommonModule,MatInputModule,MatIconModule,MatFormFieldModule,MatButtonModule],
+  imports: [RouterOutlet,MatButtonToggleModule,FormsModule,CommonModule,MatInputModule,MatIconModule,MatFormFieldModule,MatButtonModule,NgIconComponent],
+  providers:[provideIcons({
+    hugeGithub01,
+    hugeLeetcode,
+    hugeNewTwitter,
+    hugeLinkedin02
+  })],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
 export class AppComponent {
-chosenLink ?:string = "home";
-name?:string;
-email?:string;
+
+  chosenLink ?:string = "home";
+  name?:string;
+  email?:string;
   toggleButtonArray =[
     {
       "value":"home",
@@ -36,5 +49,29 @@ email?:string;
       "value":"contact",
       "title":"Contact"
     }
-  ] 
+  ]; 
+  socialIconsArray = [
+    {
+      localeName:"X(twitter)",
+      iconName:"hugeNewTwitter",
+      onClick:""
+    },
+    {
+      localeName:"LinkedIn",
+      iconName:"hugeLinkedin02",
+      onClick:""
+    },
+    {
+      localeName:"Github",
+      iconName:"hugeGithub01",
+      onClick:""
+    },
+    {
+      localeName:"Leetcode",
+      iconName:"hugeLeetcode",
+      onClick:""
+    },
+
+  ];
+
 }
